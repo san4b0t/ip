f5f667d1a8cc4aef58a8051e0cba061ba5be4ab3
san4b0t <sanfo.bt@gmail.com>
2025-08-22 16:00:21 +0800

    changed chatbot name and implement skeletal version

---
+++ src/main/java/Sanbot.java
@@ -0,0 +1,9 @@
+public class Sanbot {
+    public static void main(String[] args) {
+
+        String greeting ="Hello! I'm Sanbot\nWhat can I do for you?\n";
+        String bye = "Bye. Hope to see you again soon!";
+
+        System.out.println(greeting + "\n" + bye);
+    }
+}
