ff1c8a165d7d9b8766c94db4899cd81a497d23a0
san4b0t <sanfo.bt@gmail.com>
2025-08-22 17:10:02 +0800

    echos commands

--- src/main/java/Sanbot.java
+++ src/main/java/Sanbot.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Sanbot {
     public static void main(String[] args) {
 
         String greeting ="Hello! I'm Sanbot\nWhat can I do for you?\n";
         String bye = "Bye. Hope to see you again soon!";
 
-        System.out.println(greeting + "\n" + bye);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(greeting);
+
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(bye);
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
+        scanner.close();
     }
 }
