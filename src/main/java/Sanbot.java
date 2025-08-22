a68c030b2d6900e24cbcb893a49eb5d23703407a
san4b0t <sanfo.bt@gmail.com>
2025-08-22 18:26:07 +0800

    code clean up

--- src/main/java/Sanbot.java
+++ src/main/java/Sanbot.java
@@ -2,36 +2,44 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Sanbot {
+
+    private static ArrayList<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
 
         String greeting ="Hello! I'm Sanbot\nWhat can I do for you?\n";
-        String bye = "Bye. Hope to see you again soon!";
-
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println(greeting);
 
-        String input;
-        ArrayList<String> list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
 
-        while (true) {
-            input = scanner.nextLine();
-
-            if (input.equals("bye")) {
-                System.out.println(bye);
-                break;
-            } else if (input.equals("list")){
-                int index = 1;
-                for (String i : list) {
-                    System.out.println(index + ". " + i);
-                    index++;
-                }
-            } else {
-                list.add(input);
-                System.out.println("added: " + input);
-            }
+        while (!exit) {
+            exit = handleInput(scanner.nextLine());
         }
 
         scanner.close();
+    }
+
+    private static boolean handleInput(String input) {
+        String bye = "Bye. Hope to see you again soon!";
+
+        if (input.equals("bye")) {
+            System.out.println(bye);
+            return true;
+        } else if (input.equals("list")){
+            printListItems();
+        } else {
+            list.add(input);
+            System.out.println("added: " + input);
+        }
+        return false;
+    }
+
+    private static void printListItems() {
+        int index = 1;
+        for (String item : list) {
+            System.out.println(index + ": " + item);
+            index++;
+        }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,13 @@
+public class Task {
+    private String name;
+    private boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+}
