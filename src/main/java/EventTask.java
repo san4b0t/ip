48e02b5606de905e63c54028ec9cd817ea65802c
san4b0t <sanfo.bt@gmail.com>
2025-08-24 13:50:54 +0800

    added event task class

---
+++ src/main/java/EventTask.java
@@ -0,0 +1,15 @@
+public class EventTask extends Task{
+    private String start;
+    private String end;
+
+    public EventTask(String name, String start, String end) {
+        super(name);
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "[E] " + this.status() + String.format("(from: %s to: %s)", this.start, this.end);
+    }
+}
