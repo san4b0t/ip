5b51a7e464100b9c6a89292c95837513eb541dd6
san4b0t <sanfo.bt@gmail.com>
2025-09-09 15:19:53 +0800

    Add JavaDoc comments to tasklist and ui packages

--- src/main/java/sanbot/ui/Ui.java
+++ src/main/java/sanbot/ui/Ui.java
@@ -2,66 +2,121 @@ package sanbot.ui;
 
 import sanbot.task.Task;
 
+/**
+ * Utility class for handling all user interface output messages.
+ */
 public class Ui {
 
+    /**
+     * Displays the welcome greeting message.
+     */
     public static void greeting() {
-        String greeting = "Hello! I'm sanbot.Sanbot\nWhat can I do for you?\n";
+        String greeting = "Hello! I'm Sanbot\nWhat can I do for you?\n";
         System.out.println(greeting);
     }
 
+    /**
+     * Displays confirmation message for marking task as done.
+     *
+     * @param task the task that was marked as done
+     */
     public static void markDone(Task task) {
         System.out.println("Nice! I have marked this task as done:\n" + task.toString());
     }
 
+    /**
+     * Displays error message for invalid mark done command format.
+     */
     public static void markDoneIndexError() {
         System.out.println("Oops! It seems like you are trying to mark your task done\n" +
                 "but you missed out the task number\nor maybe forgot to add a space before the number?");
     }
 
+    /**
+     * Displays error message for invalid mark done task index.
+     */
     public static void markDoneInvalidIndex() {
         System.out.println("Oops! It seems like you are trying to mark your task done\n" +
                 "but you have entered an invalid task index");
     }
 
+    /**
+     * Displays confirmation message for marking task as not done.
+     *
+     * @param task the task that was marked as not done
+     */
     public static void markNotDone(Task task) {
         System.out.println("OK, I've marked this task as not done yet:\n" + task.toString());
     }
 
+    /**
+     * Displays error message for invalid mark not done command format.
+     */
     public static void markNotDoneIndexError() {
         System.out.println("Oops! It seems like you are trying to mark your task as not done\n"
                 + "but you missed out the task number\nor maybe forgot to add a space before the number?");
     }
 
+    /**
+     * Displays error message for invalid mark not done task index.
+     */
     public static void markNotDoneInvalidIndex() {
         System.out.println("Oops! It seems like you are trying to mark your task as not done\n" +
                 "but you have entered an invalid task index");
     }
 
+    /**
+     * Displays confirmation message for deleting a task.
+     *
+     * @param removed the task that was deleted
+     * @param count the number of remaining tasks in the list
+     */
     public static void delete(Task removed, int count) {
         System.out.println("OK, I have removed this task from the list:\n" + removed);
         System.out.printf("Now you have %d task(s) in the list%n", count);
     }
 
+    /**
+     * Displays error message for invalid delete command format.
+     */
     public static void deleteIndexError() {
         System.out.println("Oops! It seems like you are trying to delete a task\n"
                 + "but you missed out the task number\nor maybe forgot to add a space before the number?");
     }
 
+    /**
+     * Displays error message for invalid delete task index.
+     */
     public static void deleteInvalidIndex() {
         System.out.println("Oops! It seems like you are trying to delete a task\n" +
                 "but you have entered an invalid task index");
     }
 
+    /**
+     * Displays confirmation message for adding a todo task.
+     *
+     * @param task the todo task that was added
+     * @param count the total number of tasks in the list
+     */
     public static void todo(Task task, int count) {
         System.out.println("Got it. I have added this task:\n" + task);
         System.out.printf("Now you have %d task(s) in the list%n", count);
     }
 
+    /**
+     * Displays confirmation message for adding a deadline task.
+     *
+     * @param task the deadline task that was added
+     * @param count the total number of tasks in the list
+     */
     public static void deadline(Task task, int count) {
         System.out.println("Got it. I have added this task:\n" + task);
         System.out.printf("Now you have %d task(s) in the list%n", count);
     }
 
+    /**
+     * Displays error message for invalid deadline command format.
+     */
     public static void deadlineIndexError() {
         System.out.println("I see you are trying to add a task with a deadline.\n"
                 + "it seems you did not use the '/by' flag properly\n"
@@ -69,11 +124,20 @@ public class Ui {
                 + "deadline <task> /by <deadline>");
     }
 
+    /**
+     * Displays confirmation message for adding an event task.
+     *
+     * @param task the event task that was added
+     * @param count the total number of tasks in the list
+     */
     public static void event(Task task, int count) {
         System.out.println("Got it. I have added this task:\n" + task);
         System.out.printf("Now you have %d task(s) in the list%n", count);
     }
 
+    /**
+     * Displays error message for invalid event command format.
+     */
     public static void eventIndexError() {
         System.out.println("I see you are trying to add an event.\n"
                 + "it seems you did not use the '/from' and '/to' flags properly\n"
