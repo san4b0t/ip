f58a89a00dd6e1ac3f3f0922832b26f3b87d23c1
san4b0t <sanfo.bt@gmail.com>
2025-09-18 15:47:13 +0800

    Add fix to code style

--- src/main/java/sanbot/command/ExitAppCommand.java
+++ src/main/java/sanbot/command/ExitAppCommand.java
@@ -1,30 +1,40 @@
 package sanbot.command;
 
+import java.util.ArrayList;
+
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.util.Duration;
+
 import sanbot.storage.Storage;
 import sanbot.task.Task;
 import sanbot.ui.Ui;
 
-import java.util.ArrayList;
-
 /**
- * Command to close the chatbot window
+ * Represents the command to close the chatbot window.
  */
 public class ExitAppCommand implements Command {
+
     /**
+     * The time delay before exiting the app.
+     */
+    private static final double EXIT_DELAY_SECONDS = 4;
+
+    /**
+     * Command to exit the app and display exit message to user.
      *
-     * @param input the user input string that triggered this command
-     * @param taskList the list of tasks
-     * @return response to inform user that currently exiting chatbot
+     * @param input the user input string that triggered this command.
+     * @param taskList the list of tasks.
+     * @return response to inform user that currently exiting chatbot.
      */
     public String execute(String input, ArrayList<Task> taskList) {
         Storage.saveTasks(taskList);
-        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(4), e -> {
-            System.exit(0);
-        }));
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(EXIT_DELAY_SECONDS),
+                e -> System.exit(0)));
+
         timeline.play();
+
         return Ui.exitApp();
     }
 }
--- src/main/java/sanbot/command/MarkNotDoneCommand.java
+++ src/main/java/sanbot/command/MarkNotDoneCommand.java
@@ -1,29 +1,31 @@
 package sanbot.command;
 
+import java.util.ArrayList;
+
 import sanbot.task.Task;
 import sanbot.ui.Ui;
 
-import java.util.ArrayList;
-
 /**
- * Command to mark tasks as not completed by index.
+ * Represents a command to mark tasks as not completed by index.
  */
-public class MarkNotDoneCommand {
+public class MarkNotDoneCommand implements Command {
 
     /**
-     * Marks a task as not done by its position number.
+     * Executes the command to mark a task as not done by its position number.
      * Format: "unmark [task_number]"
      *
-     * @param input the user input string containing the unmark command and task number
-     * @param taskList the list of tasks containing the task to mark as not done
-     * @throws ArrayIndexOutOfBoundsException if the input format is invalid or missing the task number
-     * @throws IndexOutOfBoundsException if the specified task index is out of bounds for the task list
+     * @param input the user input string containing the unmark command and task number.
+     * @param taskList the list of tasks containing the task to mark as not done.
+     * @throws ArrayIndexOutOfBoundsException if the input format is invalid or missing the task number.
+     * @throws IndexOutOfBoundsException if the specified task index is out of bounds for the task list.
      */
     public String execute(String input, ArrayList<Task> taskList) {
-        String[] processed = input.split(" ");
+        String[] processedParts = input.split(" ");
+
         try {
-            int num = Integer.parseInt(processed[1].trim()) - 1;
+            int num = Integer.parseInt(processedParts[1].trim()) - 1;
             taskList.get(num).markAsNotDone();
+
             return Ui.markNotDone(taskList.get(num));
         } catch (ArrayIndexOutOfBoundsException e) {
             return Ui.markNotDoneIndexError();
