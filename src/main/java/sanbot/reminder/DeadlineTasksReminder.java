7211899258fcf390f41fa77fcd7fb01e3d642dcb
san4b0t <sanfo.bt@gmail.com>
2025-09-18 23:07:39 +0800

    Add minor fix to deadline task reminder

--- src/main/java/sanbot/reminder/DeadlineTasksReminder.java
+++ src/main/java/sanbot/reminder/DeadlineTasksReminder.java
@@ -8,6 +8,11 @@ import java.util.ArrayList;
 public class DeadlineTasksReminder {
     public static String remindDeadlinesMessage() {
         ArrayList<Task> deadlineTasks = Storage.getDeadlineTasks();
+
+        if (deadlineTasks.isEmpty()) {
+            return "You have no pending deadline tasks, keep it up!";
+        }
+
         StringBuilder reminder = new StringBuilder();
         reminder.append("You have the following deadline tasks due\n");
 
